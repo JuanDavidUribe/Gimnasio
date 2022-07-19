6fbf57bd26d6a390697c9d6243961f8bc8b2b805
Cristian Alejandro Idarraga <idarragacristian8@gmail.com>
2022-07-19 13:38:00 -0500

    se agregan cambios en pedir citas

--- src/main/resources/static/Formularios/js/pedir_cita.js
+++ src/main/resources/static/Formularios/js/pedir_cita.js
@@ -1,35 +1,41 @@
-const getInitialData = 'http://localhost:8080/api/disponibles/listar';
-const saveSession = 'http://localhost:8080//api/citas/guardar';
+const getInitialData = "http://localhost:8080/api/disponibles/listar";
+const saveSession = "http://localhost:8080//api/citas/guardar";
+dataHeader = `<tr>
+<th>Clase</th>
+<th>Profesor</th>
+<th>Fecha inicio</th>
+<th>Fecha fin</th>
+<th>Cupo</th>
+<th>Acciones</th>
+</tr>`;
 
-let dataTest = [
-    {
-        id: 0,
-        id_profesor: '',
-        nombre : '',
-        fecha_inicio: new Date(),
-        fecha_fin: new Date(),
-        cupo: 0
-    },
-    {
-        id: 0,
-        id_profesor: '',
-        nombre : '',
-        fecha_inicio: new Date(),
-        fecha_fin: new Date(),
-        cupo: 0
-    },
-    {
-        id: 0,
-        id_profesor: '',
-        nombre : '',
-        fecha_inicio: new Date(),
-        fecha_fin: new Date(),
-        cupo: 0
-    }
-]
+const getData = async () => {
+  let response = [];
+  let tableText = dataHeader;
+  let table = document.querySelector("#customer");
+  let request = await fetch(getInitialData);
+  response = await request.json();
+  response.forEach((data) => {
+    tableText += generateRow(data);
+  });
+  table.innerHTML = tableText;
+};
 
-
-const getData = async () =>{
+const saveData = async (data) => {
     let request = await fetch(getInitialData);
-    let response = await request.json();
+    let response = await request.text();
+    if(response === 'OK'){
+        document.getElementById(`#row${data.id}`).style.backgroundColor = '#66B933';
+    }
 }
+
+const generateRow = (data) => {
+  return `<tr>
+        <td>${data.clase}</td>
+        <td>${data.profesor}</td>
+        <td>${data.fechaInicio}</td>
+        <td>${data.fechaFin}</td>
+        <td>${data.cupo}</td>
+        <td> <a id="row${data.id}"  class="btn" href="#" onclick="saveData(data)">Inscribirme</a><br></td>
+    </tr>`;
+};
